00a18ff0724355d8bef385476183c58eb95b2966
Michael Ricky <37214301+djtyranix@users.noreply.github.com>
2020-03-21 20:14:58 +0700

    Update Soal1.c

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -8,98 +8,114 @@
 #include <syslog.h>
 #include <string.h>
 #include <time.h>
+#include <sys/wait.h>
+#include <ctype.h>
 
-int check(struct tm tm, char* aux, char* cmd){
-  char space[4][4];
-  memset(space[0],'\0',sizeof(space[0]));
-  memset(space[1],'\0',sizeof(space[0]));
-  memset(space[2],'\0',sizeof(space[0]));
-
-  int counter = 0;
-  int digit = 0;
-  int iter = 0 ;
-  while(aux[iter] != '\0' && counter < 3)
-  {
-    if(aux[iter]==' '){
-      counter++;
-      digit = 0;
-    }
-    else {
-      space[counter][digit] = aux[iter];
-      digit++;
-    }
-    iter++;
-  }
-  strcpy(cmd,aux+iter);
-
-  char menit[3]; char jam[3]; char detik[3];
-
-  sprintf(menit, "%d", tm.tm_min);
-  sprintf(jam, "%d", tm.tm_hour);
-  sprintf(detik, "%d", tm.tm_sec);
-  printf("%s-%s-%s\n", space[0], space[1], space[2]);
-  printf("%s, %s, %s\n", detik, menit, jam);
-
-  int flag = 1;
-
-  if(strcmp(detik, space[0]) != 0 && strcmp(space[0], "*") != 0) return 0;
-  if(strcmp(menit, space[1]) != 0 && strcmp(space[1], "*") != 0) return 0;
-  if(strcmp(jam, space[2]) != 0 && strcmp(space[2], "*") != 0) return 0;
-
-  return flag;
-}
 
 
 int main(int argc, char *argv[]) {
-  //if(argc != 3){
-    //perror("ERROR: argumen yang diberikan tidak sesuai");
-    //return -1;
-  //}
-  char input[1000];
-  sprintf(input, "%s %s %s %s", argv[1], argv[2], argv[3], argv[4]);
-
-  pid_t pid, sid;
-
-  pid = fork();
-
-  if (pid < 0) {
+  if(argc != 5){
+    printf("ERROR: argumen yang diberikan tidak sesuai\n");
     exit(EXIT_FAILURE);
   }
-
-  if (pid > 0) {
-    exit(EXIT_SUCCESS);
-  }
-
-  umask(0);
-
-  sid = setsid();
-
-  if (sid < 0) {
-    exit(EXIT_FAILURE);
-  }
-
-  if ((chdir("/home/yaniarpe/modul2/")) < 0) {
-    exit(EXIT_FAILURE);
-  }
-
-  close(STDIN_FILENO);
-  close(STDOUT_FILENO);
-  close(STDERR_FILENO);
-
-  while(1) {
-    // cek waktu sekarang
-    time_t t=time(NULL);
-	  struct tm tm = *localtime(&t);
-
-    int menit = tm.tm_min;
-    int jam = tm.tm_hour;
-    int detik = tm.tm_sec;
-    char command[1000];
-    if(check(tm,input,command)){
-      char *argv[] = {"bash", command, NULL};
-      //execv("bin/bash", argv);
+  else{
+    int times[4];
+    int i;
+    char command[1024];
+    for(i = 1; i <= 3; i++){
+      if(strlen(argv[i]) > 2){
+        printf("ERROR: argumen yang diberikan tidak sesuai\n");
+        exit(EXIT_FAILURE);
+      }
+      else if(strcmp(argv[i], "*") == 0){
+        times[i] = -1; //for every (sec/min/hour)
+      }
+      else{
+        int j, alpha = 0;
+        for(j = 0 ; j < (strlen(argv[i]) + 1); j++){
+          if(isalpha(argv[i][j])){
+            alpha = 1;
+            break;
+          }
+        }
+        if(alpha == 1){
+          printf("ERROR: argumen yang diberikan tidak sesuai\n");
+          exit(EXIT_FAILURE);
+        }
+        else{
+        times[i] = atoi(argv[i]);
+          if(times[i]<0){
+            printf("ERROR: argumen yang diberikan tidak sesuai\n");
+            exit(EXIT_FAILURE);
+          }
+        }
+      }
     }
-    sleep(1);
+    if (times[1] > 59){
+      printf("ERROR: argumen yang diberikan tidak sesuai\n");
+      exit(EXIT_FAILURE);
+    }
+    if (times[2] > 59){
+      printf("ERROR: argumen yang diberikan tidak sesuai\n");
+      exit(EXIT_FAILURE);
+    }
+    if (times[3] > 23){
+      printf("ERROR: argumen yang diberikan tidak sesuai\n");
+      exit(EXIT_FAILURE);
+    }
+    strcpy(command, argv[4]);
+
+      pid_t pid, sid;
+
+      pid = fork();
+
+      if (pid < 0) {
+        exit(EXIT_FAILURE);
+      }
+
+      if (pid > 0) {
+        exit(EXIT_SUCCESS);
+      }
+
+      umask(0);
+
+      sid = setsid();
+
+      if (sid < 0) {
+        exit(EXIT_FAILURE);
+      }
+
+      if ((chdir("/home/yaniarpe/modul2/")) < 0) {
+        exit(EXIT_FAILURE);
+      }
+
+      close(STDIN_FILENO);
+      close(STDOUT_FILENO);
+      close(STDERR_FILENO);
+
+    while(1) {
+      time_t t;
+  	  struct tm *tm;
+      t = time(NULL);
+      tm = localtime(&t);
+      //check time
+      if((times[1] == -1 || tm->tm_sec == times[1]) && (times[2] == -1 || tm->tm_min == times[2]) && (times[3] == -1 || tm->tm_hour == times[3])){
+        pid_t child_id;
+        int status;
+        child_id = fork();
+
+        if (child_id < 0) {
+          exit(EXIT_FAILURE);
+        }
+        if (child_id == 0){
+          char *argvv[] = {"bash", command, NULL};
+          execv("/bin/bash", argvv);
+        }
+        else{
+          wait(NULL);
+        }
+      }
+      sleep(1); //interval every second
+    }
   }
-  exit(EXIT_SUCCESS);
 }
